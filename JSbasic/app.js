981ea22a49f1d0e088b12489e7fa2b68c938eacb
sebahopp <sebahopp.kodehode@gmail.com>
2023-06-07 23:17:04 +0200

    commit2

---
+++ JSbasic/app.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+let myLet = 2;
+const myConst = 4;
+console.log(myConst + myLet);
+let myArray = ["potato", "tomato", "oragne?"];
+console.log(myArray);
+let myString = "I'm a string, WOOOOOOOOOO!";
+console.log(myString);
+const myBoolean = true;
+alert("BOO!");
